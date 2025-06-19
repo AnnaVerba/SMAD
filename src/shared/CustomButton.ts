658299e02ff35333eff7520b965c8e81640c8dcb
Maksym Logvyniuk <maksym.logvyniuk@fivesysdev.com>
2025-06-19 14:22:28 +0300

    fix: remove custom button

--- src/shared/CustomButton.ts
+++
@@ -1,6 +0,0 @@
-import { Button, styled } from '@mui/material';
-
-export const CustomButton = styled(Button)(({ theme }) => ({
-  backgroundColor: theme.palette.primary.main,
-  color: theme.palette.text.primary,
-}));
